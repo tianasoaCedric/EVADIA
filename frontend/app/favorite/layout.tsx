9b07943e994ab367f42c03b3ea23c05e10b8556b
nantenaina98 <nanterakotonirina98@gmail.com>
2026-05-29 20:37:13 +0300

    Layout bdb

--- frontend/app/favorite/layout.tsx
+++ frontend/app/favorite/layout.tsx
@@ -1,3 +1,6 @@
+
+import Header from '@/app/components/molecules/Header'
+
 export default function DarkLayout({
   children,
 }: {
@@ -5,6 +8,7 @@ export default function DarkLayout({
 }) {
   return (
     <main className="min-h-screen">
+      <Header/>
       {children}
     </main>
   )
