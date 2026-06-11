cc0605ffb3c8e71848740122092f06c8d58d1bba
nantenaina98 <nanterakotonirina98@gmail.com>
2026-06-11 11:30:44 +0300

    .

--- evadia-app/lib/api.ts
+++ evadia-app/lib/api.ts
@@ -4,7 +4,7 @@ import * as SecureStore from "expo-secure-store";
 // En développement : IP de votre PC sur le réseau local
 // Emulateur Android : http://10.0.2.2:8000
 // Appareil physique / iOS : http://192.168.1.220:8000
-export const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL ?? "http://192.168.1.220:8000";
+export const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL ?? "http://192.168.10.104:8000";
 
 export const TOKEN_KEY = "evadia_auth_token";
 
